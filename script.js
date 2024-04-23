6ae0a5f68d20b665fe330dc4cbeacf9229ca0253
jagrutiranalkar16 <jagrutiranalkar94@gmail.com>
2024-04-23 17:44:57 +0200

    counter

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function decreamentCounter(){
     //console.log(counter);
 }
 
+function clearResult(){
+    document.getElementById("bt-result").innerText = '0';
+}
+
+
 /*const increamentCounter=()=>
 counter=counter+1;
 document.getElementById("bt-result").innerText = counter;
@@ -22,51 +27,4 @@ document.getElementById("bt-result").innerText = counter;*/
 
 //document.getElementById("incrament-Button").addEventListener("click", increamentCounter);
 //document.getElementById("decreament-Button").addEventListener("click",decreamentCounter);
-
-//capital uppercase first letter
-//week5
-//const str="kasia";
-function uppercaseFirstLetter(str){
-    //let firstLetter= str.charAt[0];
-    let firstLetter=(str.slice(0,1)).toUpperCase();
-    
-    //let secondLetter=str.substring(1,str.length);
-    let secondLetter=str.slice(1);
-
-    //let toupper=firstLetter.toUpperCase();
-    console.log(firstLetter.toUpperCase());
-    console.log(secondLetter);
-    //console.log(toupper);
-
-    console.log(firstLetter+secondLetter);
-    console.log("---------------");
-}
-console.log(uppercaseFirstLetter("kasia")=="Kasia");
-console.log(uppercaseFirstLetter("kasia")=="KAsia");
-console.log(uppercaseFirstLetter("1hello")=="1hell");
-
-//create button with Javascript
-const newButton = document.createElement('button');
-newButton.textContent = 'Click here';
-document.body.appendChild(newButton);
-newButton.style="width:100px;margin-top: 500px";
-
-
-
-//assignment change button color
-const addCart = document.createElement('button');
-addCart.textContent = 'Add to cart';
-document.body.appendChild(addCart);
-addCart.style="background-color:black;color:white;margin-top: 500px;font-size: 36px;";
-
-addCart.addEventListener("click", changeStyles);
-
-function changeStyles(){
-    //
-     addCart.style="background-color:green;color:white;margin-top: 500px;font-size: 36px;";
-
-    addCart.className=".changecolor";
-    //addCart.style=".changecolor";
-}
-
 
